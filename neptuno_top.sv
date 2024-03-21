d14f1a366aab5a9c721c15472c074c3edfc7fa64
turri21 <154733583+turri21@users.noreply.github.com>
2024-03-21 20:34:43 +0200

    Update neptuno_top.sv

--- neptuno_top.sv
+++ neptuno_top.sv
@@ -35,7 +35,7 @@ reg led;
       else if (timer == 32'd99_999_999)     //The crystal oscillator used in the development board is 50MHz, counting in 2 seconds (50M*2-1=99_999_999)
           timer <= 0;                       //The counter counts up to 2 seconds and is cleared to zero.
       else
-		    timer <= timer + 1'b1;            //Counter increments by 1
+		    timer <= timer + 1'b1;  //Counter increments by 1
     end
 
 
@@ -46,7 +46,7 @@ reg led;
   always @(posedge clk or negedge rst_n)   //Detect the rising edge of clock and the falling edge of reset
     begin
       if (~rst_n)                          //Reset signal is active low
-          led <= 1'b1;                  	 //LED light output is high
+          led <= 1'b1;                     //LED light output is high
       else if (timer == 32'd49_999_999)    //The counter counts up to 1 second,
           led <= 1'b0;                     //LED lights up
       else if (timer == 32'd99_999_999)    //The counter counts up to 2 seconds,
