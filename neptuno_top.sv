22c7987c8a8a2f0feff37ef22c5eb979183b14ce
turri21 <154733583+turri21@users.noreply.github.com>
2024-03-21 20:48:27 +0200

    Update neptuno_top.sv

--- neptuno_top.sv
+++ neptuno_top.sv
@@ -28,7 +28,7 @@ reg led;
 // Counter counting: loop counting 0~2 seconds
 //===========================================================================
 
-  always @(posedge clk or negedge rst_n)    //Detect the rising edge of clock and the falling edge of reset
+always @(posedge clk or negedge rst_n)    //Detect the rising edge of clock and the falling edge of reset
 
     begin
       if (~rst_n)                           //Reset signal is active low
@@ -36,7 +36,7 @@ reg led;
       else if (timer == 32'd99_999_999)     //The crystal oscillator used in the development board is 50MHz, counting in 2 seconds (50M*2-1=99_999_999)
           timer <= 0;                       //The counter counts up to 2 seconds and is cleared to zero.
       else
-		    timer <= timer + 1'b1;  //Counter increments by 1
+	  timer <= timer + 1'b1;  //Counter increments by 1
     end
 
 
@@ -44,7 +44,7 @@ reg led;
 // LED light control
 //===========================================================================
 
-  always @(posedge clk or negedge rst_n)   //Detect the rising edge of clock and the falling edge of reset
+always @(posedge clk or negedge rst_n)   //Detect the rising edge of clock and the falling edge of reset
     begin
       if (~rst_n)                          //Reset signal is active low
           led <= 1'b1;                     //LED light output is high
